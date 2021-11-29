3d26b30ce7929709d99e1150d2d5b2c69522c703
My <mykellner@gmail.com>
2021-11-29 10:30:57 +0100

    duplicator installed

--- wp-config.php
+++ wp-config.php
@@ -39,16 +39,16 @@ $_SERVER["HTTPS"] = "on";
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'ashish_naturkompanitva' );
+define( 'DB_NAME', "wp_project" );
 
 /** MySQL database username */
-define( 'DB_USER', 'ashish_naturkompanitva' );
+define( 'DB_USER', "root" );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'ZfOTP6RyrBMF' );
+define( 'DB_PASSWORD', "root" );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', "localhost" );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
