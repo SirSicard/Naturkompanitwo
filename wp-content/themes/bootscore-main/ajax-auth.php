e4d2cea2ab7012bec530a4e37a93027d4cc83b56
Elle <eleonormcdowell@gmail.com>
2021-12-02 23:10:37 +0100

    Small changes in forms

--- wp-content/themes/bootscore-main/ajax-auth.php
+++ wp-content/themes/bootscore-main/ajax-auth.php
@@ -9,10 +9,10 @@
 	<a class="text-link w-100" href="<?php echo wp_lostpassword_url(); ?>">
 	<?php _e('Glömt ditt lösenord?','booscore') ?>
 	</a>
-	<input class="submit_button w-100 btn btn-dark my-3" type="submit" value="<?php _e('Logga In','bootscore')?>"></input>
+	<input class="submit_button w-100 btn btn-dark my-3" type="submit" value="<?php _e('Logga in','bootscore')?>"></input>
 
 	<h3><?php _e('Ny användare?', 'bootscore') ?>
-		<a id="pop_signup" href=""><?php _e('Skapa ett Konto', 'bootscore') ?></a>
+		<a id="pop_signup" href=""><?php _e('Skapa ett konto', 'bootscore') ?></a>
 	</h3>
 	<hr />
 
@@ -23,19 +23,30 @@
 </form>
 
 <form id="register" class="ajax-auth" action="register" method="post">
-	<h3><?php _e('Har du redan ett konto?','bootscore') ?> <a id="pop_login" href=""><?php _e('Logga in', 'bootscore') ?></a></h3>
+
+	<h3> <?php _e('Har du redan ett konto?','bootscore') ?> 
+		<a id="pop_login" href=""><?php _e('Logga in', 'bootscore') ?></a>
+	</h3>
+
 	<hr />
-	<h1><?php _e('Bli Medlem','bootscore')?></h1>
+
+	<h1> <?php _e('Bli Medlem','bootscore')?> </h1>
 	<p class="status"></p>
 	<?php wp_nonce_field('ajax-register-nonce', 'signonsecurity'); ?>
 	<label for="signonname"><?php _e('Användarnamn','bootscore')?></label>
-	<input id="signonname" type="text" name="signonname" class="required w-100">
+		<input id="signonname" type="text" name="signonname" class="required w-100">
 	<label for="email"><?php _e('Email','bootscore') ?></label>
-	<input id="email" type="text" class="required email w-100" name="email">
+		<input id="email" type="text" class="required email w-100" name="email">
 	<label for="signonpassword"><?php _e('Lösenord','bootscore')?></label>
-	<input id="signonpassword" type="password" class="required w-100" name="signonpassword">
+		<input id="signonpassword" type="password" class="required w-100" name="signonpassword">
 	<label for="password2"><?php _e('Bekräfta Lösenord','bootscore') ?></label>
-	<input type="password" id="password2" class="required w-100" name="password2">
-	<input class="submit_button btn btn-dark w-100" type="submit" value="Bli Medlem"></input>
-	<a class="close" href=""><i class="fas fa-window-close fa-lg"></i></a>
+		<input type="password" id="password2" class="required w-100" name="password2">
+	<input class="submit_button btn btn-dark w-100 my-3" type="submit" value="<?php _e('Bli medlem','bootscore')?>"></input>
+
+	<div class="fb-login-button btn w-100" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="true">
+	</div>
+
+	<a class="close" href="">
+		<i class="fas fa-window-close fa-lg"></i>
+	</a>
 </form>
