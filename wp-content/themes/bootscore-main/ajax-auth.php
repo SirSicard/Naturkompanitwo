bca59741844135ad118125c9102c8c5556d7746b
Elle <eleonormcdowell@gmail.com>
2021-12-03 13:23:06 +0100

    Added FB Login Button

--- wp-content/themes/bootscore-main/ajax-auth.php
+++ wp-content/themes/bootscore-main/ajax-auth.php
@@ -24,12 +24,11 @@
 
 	<!-- <div class="fb-login-button btn w-100" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="true"></div> -->
 
-	<a href="https://naturkompanitva.ashish.se/wp-login.php?loginSocial=facebook" data-plugin="nsl" data-action="connect" data-redirect="current" data-provider="facebook" data-popupwidth="600" data-popupheight="679">
-		<img src="Image url" alt="" />
-	</a>
-
-
-
+	<?php
+		if(class_exists('NextendSocialLogin', false)){
+			echo NextendSocialLogin::renderButtonsWithContainer();
+		}
+	?>
 
 	<a class="close" href="">
 		<i class="fas fa-window-close fa-lg"></i>
