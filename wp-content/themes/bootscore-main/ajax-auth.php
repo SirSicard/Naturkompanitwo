a310468748bd924d46e78257aead9bb49bd5e70a
My <mykellner@gmail.com>
2021-12-15 13:42:07 +0100

    facebook login try again - MY och Elle

--- wp-content/themes/bootscore-main/ajax-auth.php
+++ wp-content/themes/bootscore-main/ajax-auth.php
@@ -1,52 +1,8 @@
-<script>
 
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-  console.log('statusChangeCallback');
-  console.log(response);                   // The current login status of the person.
-  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-	testAPI();  
-  } else {                                 // Not logged into your webpage or we are unable to tell.
-	document.getElementById('status').innerHTML = 'Please log ' +
-	  'into this webpage.';
-  }
-}
-
-
-function checkLoginState() {               // Called when a person is finished with the Login Button.
-  FB.getLoginStatus(function(response) {   // See the onlogin handler
-	statusChangeCallback(response);
-  });
-}
-
-
-window.fbAsyncInit = function() {
-  FB.init({
-	appId      : '519677222339502',
-	cookie     : true,                     // Enable cookies to allow the server to access the session.
-	xfbml      : true,                     // Parse social plugins on this webpage.
-	version    : 'v12.0'           // Use this Graph API version for this call.
-  });
-
-
-  FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-	statusChangeCallback(response);        // Returns the login status.
-  });
-};
-
-function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-  console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
-	console.log('Successful login for: ' + response.name);
-	document.getElementById('status').innerHTML =
-	  'Thanks for logging in, ' + response.name + '!';
-  });
-}
-
-</script>
 <!-- The JS SDK Login Button -->
 
 <!-- Load the JS SDK asynchronously -->
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+
 
 <form id="login" class="ajax-auth" action="login" method="post">
 	<h1>
@@ -72,22 +28,16 @@ function testAPI() {                      // Testing Graph API after login.  See
 
 	<hr />
 
-	<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
+	<!-- <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
+ -->
 
+	<!-- The JS SDK Login Button -->
+	
 	<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-	</fb:login-button>
+    </fb:login-button>
 
 	<div id="status">
 	</div>
-
-
-	<!-- The JS SDK Login Button -->
-	<!-- 
-	<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-	</fb:login-button>
-
-	<div id="status">
-	</div> -->
 
 	<a class="close" href="">
 		<i class="fas fa-window-close fa-lg"></i>
